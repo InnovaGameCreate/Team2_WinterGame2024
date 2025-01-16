8294c204c17f5f8250635bf009a332ba7143c9cc
[tyunousause] <tyunousause@gmail.com>
2025-01-17 05:39:44 +0900

    アイテム種類のEnum作成

--- Assets/Scripts/Scenes/InGame/GameLogic/EnumInGameLogic.cs
+++ Assets/Scripts/Scenes/InGame/GameLogic/EnumInGameLogic.cs
@@ -10,3 +10,28 @@ enum FlaskType : byte
     Random,
     None,
 }
+
+enum ItemType : byte
+{ 
+    Default,
+    /// <summary>
+    /// ƒAƒƒG
+    /// </summary>
+    Aloe,
+    /// <summary>
+    /// ŒŒ´
+    /// </summary>
+    Serum,
+    /// <summary>
+    /// ŒŸ¸–ò
+    /// </summary>
+    Tests,
+    /// <summary>
+    /// ‡–°–ò
+    /// </summary>
+    SleepingPill,
+    /// <summary>
+    /// ‘¶İ‚µ‚È‚¢
+    /// </summary>
+    None,
+}
